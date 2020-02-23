1e2cad5f96f1fff9f54a3baa6080b6d9cb8084cf
wilsonvolker <40080421+wilsonvolker@users.noreply.github.com>
2020-02-24 01:43:03 +0800

    incorrect content update

---
+++ precache-manifest.0fee92dcd7675ed06fb0dacb5402507b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12803f89e15e24e2616b5715c80fde7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f10676dbc5e364f0a03",
+    "url": "/static/css/main.3f25bde9.chunk.css"
+  },
+  {
+    "revision": "f0aa0c206fc385e8e4c3",
+    "url": "/static/js/2.d3f266e7.chunk.js"
+  },
+  {
+    "revision": "928e2c3a914e24d90a2bd8051e4b4d03",
+    "url": "/static/js/2.d3f266e7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f10676dbc5e364f0a03",
+    "url": "/static/js/main.e7973192.chunk.js"
+  },
+  {
+    "revision": "3c7273a078bbff7b79ad",
+    "url": "/static/js/runtime-main.5e24e384.js"
+  },
+  {
+    "revision": "0a457da386d0a73219092a95397cf4c6",
+    "url": "/static/media/KwoonChungBusHoldings_logo.0a457da3.jpg"
+  },
+  {
+    "revision": "718966cb2e5be79b88814020b335b9a7",
+    "url": "/static/media/SkyEyeBasNews.718966cb.jpg"
+  },
+  {
+    "revision": "193647f2210d902f7756d841efa4446d",
+    "url": "/static/media/atman fish banner.193647f2.jpg"
+  },
+  {
+    "revision": "99d5d9a2e4f515f083b8d514b3b15493",
+    "url": "/static/media/kongs_logo.99d5d9a2.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e2af256ff30f6397ca3ab8fe9cc695b2",
+    "url": "/static/media/logo.e2af256f.png"
+  },
+  {
+    "revision": "28c016aba3dbbe59f2dc605665701e25",
+    "url": "/static/media/lovingkids_logo.28c016ab.jpg"
+  },
+  {
+    "revision": "4278e26e8be52999b668e01ee859651c",
+    "url": "/static/media/singTaoDailyLogo.4278e26e.jpg"
+  },
+  {
+    "revision": "2f985313a6a3b472c8935fba8da83f0b",
+    "url": "/static/media/skyEyeNewsImage1.2f985313.jpg"
+  },
+  {
+    "revision": "2da768998a4c0ed0510511bccf58fe49",
+    "url": "/static/media/skyEyeNewsImage2.2da76899.jpg"
+  },
+  {
+    "revision": "8ebe8060f7cd3d1bbcf482174780aa3f",
+    "url": "/static/media/skyEyeNewsImage3.8ebe8060.jpg"
+  },
+  {
+    "revision": "99c7f58af8e11952c4baa9f63c495af5",
+    "url": "/static/media/vtcLogo.99c7f58a.jpg"
+  },
+  {
+    "revision": "e2af256ff30f6397ca3ab8fe9cc695b2",
+    "url": "/static/media/webpage_screenshot.e2af256f.png"
+  }
+]);
